39615e779047dd79c00b17ee03a36029a5f484e7
u04 <you@example.com>
2019-12-03 15:05:15 -0500

    added guards in level2

---
+++ Assets/Scripts/Guard.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guard : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject lazerPrefab;
+    public float speed = 2f;
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.right = target.position - transform.position;
+        Shoot();
+    }
+    void Shoot()
+    {
+        Instantiate(lazerPrefab, firePoint.position, firePoint.rotation);
+    }
+}
